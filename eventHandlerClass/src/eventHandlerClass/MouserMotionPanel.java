20821f5cc638a16521d13b000b9a13d6c2f27025
rhgo1749 <rhgo1749@naver.com>
2018-11-20 14:21:20 +0900

    make MouseMotionPanel Class change main panel to MouseMotionPanel add DrawListener class

--- eventHandlerClass/src/eventHandlerClass/MouserMotionPanel.java
+++ eventHandlerClass/src/eventHandlerClass/MouserMotionPanel.java
@@ -3,6 +3,9 @@ package eventHandlerClass;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
@@ -11,15 +14,69 @@ import java.awt.*;
 public class MouserMotionPanel extends JPanel{
 	
 	private Point pt1,pt2;
-	
+	private DrawListener myListener;
 	
 	public MouserMotionPanel() {
 		setPreferredSize(new Dimension(600,600));
 		setBackground(Color.white);
 		
-		pt1 = new Point();
-		pt1 = new Point();
-		
+		myListener = new DrawListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
+	}
+	
+	
+	public void paintCompoenet(Graphics g) {
+		super.paintComponent(g);
+		g.drawLine(pt1.x, pt1.y, pt2.x, pt2.y);
+	}
+	
+	private class DrawListener implements MouseListener, MouseMotionListener{
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("in");
+			pt1 = arg0.getPoint();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("out");
+			pt2 = arg0.getPoint();
+			repaint();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 }
